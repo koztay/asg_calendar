e520c59e67dc2c7da71de124a58292a4d280a133
badn3wz <lukaszburdzy@gmail.com>
2015-11-23 18:47:28 +0100

    first steps toward frontend OHMYGODOHMYGODOHMYGODPHMAHGOD!!! FINALLY!!!

--- asg/static/js/project.js
+++ asg/static/js/project.js
@@ -1,1 +1,29 @@
 /* Project specific Javascript goes here. */
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('ListCtrl', function ($scope, $http) {
+	console.log('controller loaded')
+	$scope.getEvents = function(){
+		$http.get('/api/events/').success(function(response){
+			$scope.events = response.results;
+		});
+	}
+
+
+
+
+	// $scope.initialize = function(data){
+	// 	$scope.initData = data;
+	// };
+
+	// $scope.loadItems = function() {
+	// 	$scope.items = $http.get('api/events').then(function(response){
+	// 		return response.data;
+	// 	});
+
+	// };
+
+	// $scope.loadItems();
+
+
+});
