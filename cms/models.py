e7ad44ed607fcfdfeb9db88bfc7972e84aab0f17
lburdzy <lukaszburdzy@gmail.com>
2016-03-17 19:58:21 +0100

    Events w wagtailu?

--- cms/models.py
+++ cms/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils import timezone
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
@@ -9,4 +11,26 @@ class HomePage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full")
+    ]
+
+
+class EventPage(Page):
+    description = RichTextField(blank=True)
+    poster = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    datetime = models.DateTimeField(default=timezone.now)
+    link = models.URLField(blank=True)
+    is_open = models.BooleanField(default=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('description'),
+        ImageChooserPanel('poster'),
+        FieldPanel('datetime'),
+        FieldPanel('link'),
+        FieldPanel('is_open'),
     ]
