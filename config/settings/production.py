7696696c80e029acf390259d2eafa59500184fe6
Lukasz Burdzy <lukaszburdzy@gmail.com>
2016-05-06 21:51:35 +0200

    updated mail backend

--- config/settings/production.py
+++ config/settings/production.py
@@ -102,13 +102,11 @@ MEDIA_URL = '/media/'
 DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL',
                          default='asg <noreply@example.com>')
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
-# MAILGUN_ACCESS_KEY = env('DJANGO_MAILGUN_API_KEY')
-# MAILGUN_SERVER_NAME = env('DJANGO_MAILGUN_SERVER_NAME')
+EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
+MAILGUN_ACCESS_KEY = env('DJANGO_MAILGUN_API_KEY')
+MAILGUN_SERVER_NAME = env('DJANGO_MAILGUN_SERVER_NAME')
 EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default='[asg] ')
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
-EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
-MANDRILL_API_KEY = '_FYdWLNs1EkJ8ERz9fdaFw'
 
 # TEMPLATE CONFIGURATION
 # ------------------------------------------------------------------------------
