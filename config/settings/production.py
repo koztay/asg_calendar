d90dfa9fee09985a785fa35e3bded60b102fbc81
Lukasz Burdzy <lukaszburdzy@gmail.com>
2016-08-13 14:03:46 +0200

    Fix for fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -103,7 +103,7 @@ MEDIA_URL = '/media/'
 #                          default='asg <noreply@example.com>')
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
-# MAILGUN_ACCESS_KEY = env('DJANGO_MAILGUN_API_KEY')
+MAILGUN_ACCESS_KEY = env('DJANGO_MAILGUN_API_KEY')
 # MAILGUN_SERVER_NAME = env('DJANGO_MAILGUN_SERVER_NAME')
 # EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default='[asg] ')
 # SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
